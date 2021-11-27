aad0d0a17696af4005c0abde25bf9193c0e5433a
justLazzybones <95139693+justLazzybones@users.noreply.github.com>
2021-11-27 21:05:14 +0300

    site4bro

---
+++ kontakti.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+<title>Контактные данные</title>
+<meta charset="utf-8">
+<meta name="description" content="Переезд, переезд с поднятием на этаж, перенос, уборка, вынос мусора, занос стройматериалов, выгрузка и погрузка мебели, А так же услуги разнорабочих с любым видом работы">
+<meta name="keywords" content="Переезд, переезд с поднятием на этаж, перенос, уборка, вынос мусора, занос стройматериалов, выгрузка и погрузка мебели, А так же услуги разнорабочих с любым видом работы">
+<meta name="author" content="по-Грузчик">
+<meta name="copyright" content="по-Грузчик">
+<link rel="stylesheet" type="text/css" href="/lib/main.css">
+<?php include 'lib/module/sys-meta.php';?>
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+</head>
+<body>
+<?php include 'lib/module/sys-php.php';?>
+<div class="page-con-content">
+	<div class="banner-con-container darkmode-header">
+		<div id="object-particles">
+		</div>
+		<div class="wavebar-con-container">
+			<div class="wavebar-con-wrap">
+			  <div class="wavebar-svg-object"></div>
+			  <div class="wavebar-svg-object"></div>
+			</div>
+		</div>
+		<div class="banner-con-backdrop darkmode-header">
+		</div>
+		<div align="center" class='banner-con-title'>
+				<p style="font-size: 60px; margin: 20px 0px 0px 0px">Контакты</p>
+				<p style="font-size: 30px;">Хотите или договориться о проведении работ?</p>
+			</div>
+		</div>
+	</div>
+	<div align="left"><p style="font-size: 30px;"><br>Тел.: <a href="tel: 89183733219">89183733219</a> - Андрей<br>
+	<br>Е-мэйл: <a href="mailto:Inozemtsevandrew1@gmail.com">Inozemtsevandrew1@gmail.com</a><br>
+	<br>
+	Я Вконтакте: <a href="https://vk.com/madar1n4ik">https://vk.com/madar1n4ik</a>
+	</p></div>
+	<div class="landing-concord darkmode-discord">
+    </div>
+</div>
+</div>
+</body>
+<?php include 'lib/module/ui-main-footer.php';?>
+</html>
